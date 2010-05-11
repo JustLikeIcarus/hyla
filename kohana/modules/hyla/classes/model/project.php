74c16daa02c674f6ec36ab678af7a6bfa677f9ec
Kenny Kurtz <JustLikeIcarus@gmail.com>
2010-05-11 19:58:09 -0400

    Added user_id column to projects table with migration #4.  Along with a FK constraint on users table.

--- kohana/modules/hyla/classes/model/project.php
+++ kohana/modules/hyla/classes/model/project.php
@@ -2,4 +2,18 @@
 
 class Model_Project extends ORM {
 
+	protected $_belongs_to = array('user' => array());
+
+	public function rules() 
+	{
+		$rules = array
+		(
+		    'name' => array('not_empty' => array()),
+		    'title'    => array('not_empty' => array()),
+		);
+		
+		return $rules;
+	}
+
+
 } // End Project
